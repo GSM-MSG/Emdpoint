4bf86549daea78d3f6eb33f24856aca49f138d61
baegteun <baegteun@gmail.com>
2023-03-20 19:08:29 +0900

    :sparkles: :: EmdpointError Error append

--- Sources/Emdpoint/Error/EmdpointError.swift
+++ Sources/Emdpoint/Error/EmdpointError.swift
@@ -5,3 +5,33 @@ public enum EmdpointError: Error {
     case networkError
     case notFoundOwner
 }
+
+extension EmdpointError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .encodingFailed:
+            return "failed to encoding parameters"
+
+        case .networkError:
+            return "failed to request"
+
+        case .notFoundOwner:
+            return "this error occured arc"
+        }
+    }
+}
+
+extension EmdpointError {
+    var underlyingError: Swift.Error? {
+        return nil
+    }
+}
+
+extension EmdpointError: CustomNSError {
+    public var errorUserInfo: [String: Any] {
+        var userInfo: [String: Any] = [:]
+        userInfo[NSLocalizedDescriptionKey] = errorDescription
+        userInfo[NSUnderlyingErrorKey] = underlyingError
+        return userInfo
+    }
+}
